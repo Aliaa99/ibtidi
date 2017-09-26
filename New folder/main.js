f86e7c665865d6e78ff6106dc3d97dba2a67d7e6
Aliaa Ahmed ElZohairy <aliaaahmed129@gmail.com>
2017-09-27 00:50:42 +0200

    Add files via upload
    Dashboard and table

---
+++ New folder/main.js
@@ -0,0 +1,173 @@
+
+
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+
+
+
+
+
+$(document).ready(function(){
+    $("#orange1").click(function(){
+        $("#orange1").css("color", "#f5b466")
+        $("#dot1").css("color", "#f5b466")
+$("#orange2").css("color", "gray")
+        $("#dot2").css("color", "black")
+$("#orange3").css("color", "gray")
+        $("#dot3").css("color", "black")
+
+$("#orange4").css("color", "gray")
+        $("#dot4").css("color", "black")
+$("#orange5").css("color", "gray")
+        $("#dot5").css("color", "black")
+
+
+    });
+});
+$(document).ready(function(){
+    $("#orange2").click(function(){
+        $("#orange2").css("color", "#f5b466")
+        $("#dot2").css("color", "#f5b466")
+$("#orange1").css("color", "gray")
+        $("#dot1").css("color", "black")
+$("#orange3").css("color", "#gray")
+        $("#dot3").css("color", "black")
+
+$("#orange4").css("color", "gray")
+        $("#dot4").css("color", "black")
+$("#orange5").css("color", "gray")
+        $("#dot5").css("color", "black")
+ });
+});
+
+$(document).ready(function(){
+    $("#orange3").click(function(){
+        $("#orange3").css("color", "#f5b466")
+        $("#dot3").css("color", "#f5b466")
+$("#orange1").css("color", "gray")
+        $("#dot1").css("color", "black")
+$("#orange2").css("color", "gray")
+        $("#dot2").css("color", "black")
+
+$("#orange4").css("color", "gray")
+        $("#dot4").css("color", "black")
+$("#orange5").css("color", "gray")
+        $("#dot5").css("color", "black")
+ });
+});
+$(document).ready(function(){
+    $("#orange4").click(function(){
+        $("#orange4").css("color", "#f5b466")
+        $("#dot4").css("color", "#f5b466")
+$("#orange1").css("color", "gray")
+        $("#dot1").css("color", "black")
+$("#orange3").css("color", "gray")
+        $("#dot3").css("color", "black")
+
+$("#orange2").css("color", "gray")
+        $("#dot2").css("color", "black")
+$("#orange5").css("color", "gray")
+        $("#dot5").css("color", "black")
+ });
+});
+$(document).ready(function(){
+    $("#orange5").click(function(){
+        $("#orange5").css("color", "#f5b466")
+        $("#dot5").css("color", "#f5b466")
+$("#orange1").css("color", "gray")
+        $("#dot1").css("color", "black")
+$("#orange3").css("color", "gray")
+        $("#dot3").css("color", "black")
+
+$("#orange4").css("color", "gray")
+        $("#dot4").css("color", "black")
+$("#orange2").css("color", "gray")
+        $("#dot2").css("color", "black")
+ });
+});
+$(document).ready(function(){
+    $("#example4 a").click(function(){
+        $("#example3 a").css("color", "gray")
+        $("#example3 li").css("color", "black")
+
+ });
+});
+$(document).ready(function(){
+    $("#example2 a").click(function(){
+        $("#example3 a").css("color", "gray")
+        $("#example3 li").css("color", "black")
+
+ });
+});
+$(document).ready(function(){
+    $("#example1 a").click(function(){
+        $("#example3 a").css("color", "gray")
+        $("#example3 li").css("color", "black")
+
+ });
+});
+
+
+$(document).ready(function(){
+    $("#example1 a").click(function(){
+        $("#example3 ").toggle()
+      
+
+ });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
